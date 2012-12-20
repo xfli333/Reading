6617a004b20a61e741a51703a5798518b20603c5
Seven <lixfeng.3@gmail.com>
2012-12-20 15:06:05 +0800

    DATA_DIRECTORY

---
+++ src/info/ishared/reading/AppConfig.java
@@ -0,0 +1,11 @@
+package info.ishared.reading;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Seven
+ * Date: 12-12-20
+ * Time: PM3:04
+ */
+public class AppConfig {
+    public static final String DATA_DIRECTORY="reading_data";
+}
--- src/info/ishared/reading/AppLauncherActivity.java
+++ src/info/ishared/reading/AppLauncherActivity.java
@@ -3,6 +3,7 @@ package info.ishared.reading;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.text.Html;
 import android.view.Gravity;
@@ -10,28 +11,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 import info.ishared.reading.util.PageJumpUtils;
 
+import java.io.File;
+
 public class AppLauncherActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    private TextView mHtml;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         showLoading();
+        checkDataDirectory();
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                PageJumpUtils.jump(AppLauncherActivity.this,BookListActivity.class);
+                PageJumpUtils.jump(AppLauncherActivity.this, BookListActivity.class);
                 AppLauncherActivity.this.finish();
             }
         }, 2000);
     }
 
     private void showLoading() {
-        int duration = Toast.LENGTH_LONG;
+        int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(this, "", duration);
         toast.getView().setBackground(getResources().getDrawable(R.drawable.splash));
         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
+    }
+
+    private void checkDataDirectory() {
+        File sd = Environment.getExternalStorageDirectory();
+        String path = sd.getPath() + "/" + AppConfig.DATA_DIRECTORY;
+        File file = new File(path);
+        if (!file.exists()) file.mkdir();
     }
 }
