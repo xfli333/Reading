958e320634672685b39f8c17dc405a0885981cdb
Seven <lixfeng.3@gmail.com>
2012-12-20 17:38:18 +0800

    book list

--- src/info/ishared/reading/db/BookOperator.java
+++ src/info/ishared/reading/db/BookOperator.java
@@ -2,7 +2,13 @@ package info.ishared.reading.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import info.ishared.reading.bean.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,17 +18,44 @@ import info.ishared.reading.bean.Book;
  */
 public class BookOperator {
     private DBHelper mDBHelper;
+    private SQLiteDatabase mDB;
 
     public BookOperator(Context context) {
         this.mDBHelper = DBHelper.getInstance(context);
+        this.mDB = this.mDBHelper.getMDB();
     }
 
-    public Long createBook(Book book){
+    public Long createBook(Book book) {
         this.mDBHelper.open();
         ContentValues values = new ContentValues();
         values.put(DBConfig.Book.BOOK_NUMBER, book.getBookNumber());
-        long id= this.mDBHelper.getMDB().insertOrThrow(DBConfig.Book.TABLE_NAME, null, values);
+        long id = this.mDB.insertOrThrow(DBConfig.Book.TABLE_NAME, null, values);
         this.mDBHelper.close();
         return id;
+    }
+
+    public List<Book> listAllBooks() {
+        this.mDBHelper.open();
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(DBConfig.Book.TABLE_NAME);
+        String arrColumn[] = {
+                DBConfig.Book.ID, DBConfig.Book.BOOK_NUMBER, DBConfig.Book.BOOK_NAME, DBConfig.Book.BOOK_SIZE
+        };
+        Cursor c = builder.query(this.mDB, arrColumn, null, null, null, null, null);
+        c.moveToFirst();
+        List<Book> data = new ArrayList<Book>();
+        while (!c.isAfterLast()) {
+            Book item = new Book();
+            item.setId(c.getLong(c.getColumnIndex(arrColumn[0])));
+            item.setBookNumber(c.getString(c.getColumnIndex(arrColumn[1])));
+            item.setBookName(c.getString(c.getColumnIndex(arrColumn[2])));
+            item.setBookSize(c.getString(c.getColumnIndex(arrColumn[3])));
+            data.add(item);
+            c.moveToNext();
+        }
+        c.close();
+        this.mDBHelper.close();
+        return data;
+
     }
 }
