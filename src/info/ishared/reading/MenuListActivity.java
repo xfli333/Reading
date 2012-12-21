65d6891fa6a5ac1cb4fe3c0c6e4d23f8d7374b2f
Seven <lixfeng.3@gmail.com>
2012-12-21 17:43:16 +0800

    menu

--- src/info/ishared/reading/MenuListActivity.java
+++ src/info/ishared/reading/MenuListActivity.java
@@ -2,32 +2,31 @@ package info.ishared.reading;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
  * User: Seven
  * Date: 12-12-21
- * Time: PM4:25
+ * Time: PM5:32
  */
-public class ChapterListActivity extends Activity {
+public class MenuListActivity extends Activity {
     private ListView mListView;
     private ArrayAdapter adapter;
-    private List<String> menuData=new ArrayList<String>();
+    private String chapterNumber;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.chapter_list);
-        mListView = (ListView) this.findViewById(R.id.chapter_list_view);
+        this.setContentView(R.layout.menu_list);
+        mListView = (ListView) this.findViewById(R.id.menu_list_view);
         initListViewGUI();
 
     }
@@ -35,18 +34,21 @@ public class ChapterListActivity extends Activity {
     private void initListViewGUI() {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,getData());
         mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d(AppConfig.TAG,i+"");
+            public void onClick(View view) {
+
             }
         });
     }
 
     private List<String> getData() {
-        for(int i=0;i<22;i++){
-            menuData.add("第"+i+"部分");
+        List<String> data = null;
+        try {
+            data = FileUtils.readLines(new File(AppConfig.BOOK_DIRECTORY + "/101010/" + "0/menu.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return menuData;
+        return data;
     }
 }
