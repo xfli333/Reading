8f772fe557bf2eda9842a49d0aa0f817d93f3c35
Seven <lixfeng.3@gmail.com>
2012-12-24 14:13:32 +0800

    GestureListener

--- src/info/ishared/reading/ShowContentActivity.java
+++ src/info/ishared/reading/ShowContentActivity.java
@@ -3,7 +3,11 @@ package info.ishared.reading;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import info.ishared.reading.util.ToastUtils;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -15,10 +19,13 @@ import java.io.IOException;
  * Date: 12-12-21
  * Time: 下午10:40
  */
-public class ShowContentActivity extends Activity {
+public class ShowContentActivity extends Activity implements GestureDetector.OnGestureListener {
     private TextView mTextView;
     private String fileName;
     private String bookNumber;
+    private final int FLING_MIN_DISTANCE = 50;
+    private final int FLING_SCROLL_DISTANCE = 60;
+    private GestureDetector mGestureDetector;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +34,65 @@ public class ShowContentActivity extends Activity {
 
         bookNumber = extras.getString("bookNumber");
         fileName = extras.getString("fileName");
-        mTextView =(TextView) this.findViewById(R.id.html_content);
+
+        mGestureDetector = new GestureDetector(this,this);
+
+        mTextView = (TextView) this.findViewById(R.id.html_content);
         mTextView.setText(Html.fromHtml(getFileContent()));
     }
 
-    private String getFileContent(){
+    private String getFileContent() {
         try {
-           return FileUtils.readFileToString(new File(AppConfig.BOOK_DIRECTORY+"/"+bookNumber+"/"+fileName));
+            return FileUtils.readFileToString(new File(AppConfig.BOOK_DIRECTORY + "/" + bookNumber + "/" + fileName));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "";
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent motionEvent) {
+        mGestureDetector.onTouchEvent(motionEvent);
+        return super.dispatchTouchEvent(motionEvent);
+    }
+
+//    @Override
+//    public boolean onTouch(View view, MotionEvent motionEvent) {
+//        mGestureDetector.onTouchEvent(motionEvent);
+//        return true;
+//    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent2, float v, float v2) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent1, MotionEvent motionEvent2, float v, float v2) {
+        boolean isScroll=Math.abs(motionEvent1.getY() - motionEvent2.getY()) < FLING_SCROLL_DISTANCE;
+        if (motionEvent1.getX() - motionEvent2.getX() > FLING_MIN_DISTANCE && isScroll ) {
+            // Fling left NExt
+        } else if (motionEvent2.getX() - motionEvent1.getX() > FLING_MIN_DISTANCE  && isScroll ) {
+            // Fling right
+        }
+        return false;
+    }
 }
